f765d892cc88e215cb5240c480acc05790663eba
xiamuqingliang <1471235317@qq.com>
2020-01-05 22:25:41 +0800

    修改

--- src/main/java/com/atguigu/jianfa/Dugu.java
+++ src/main/java/com/atguigu/jianfa/Dugu.java
@@ -3,5 +3,11 @@ package com.atguigu.jianfa;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
     }
 }
